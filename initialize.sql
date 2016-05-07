d4ebd53609c33d6edb2cecbce61e171a72f3c16b
randhawk <nrandhawa01@yahoo.com>
2016-05-06 23:50:26 -0700

    sample data populated

--- initialize.sql
+++ initialize.sql
@@ -1,4 +1,6 @@
 SET foreign_key_checks = 0;
+drop database if exists chess;
+create database chess;
 use chess;
  
 -- drop table gameboard_type;
@@ -12,72 +14,62 @@ use chess;
 -- drop table lobby;
  
 create table lobby(
-    player_waitlist     integer,
-        min_skill       integer,
         lobby_id        integer not null,
+        min_skill       integer,
+    player_waitlist     integer,
         primary key (lobby_id)
 );
  
 create table gameboard_type(
-    timer           float(4,2),     #minutes.seconds
-    grid_number     char(2),
-    board_color     varchar(10),
-    piece_color     char(5),
     name            varchar(15) not null,
     lobby_id        integer not null,
+    timer           varchar(8),     #hours:minutes:seconds
+    grid_number     char(2),
+    board_color     varchar(15),
+    piece_color     char(15),
     primary key (lobby_id,name),
     foreign key (lobby_id) references lobby(lobby_id)
 );
  
 create table lobby_stats(
-    pieces_taken        integer,
-        avg_gametime        float(4,2),     #hours.minutes
-    avg_movetime        float(4,2),     #minutes.seconds
+        lobby_id        integer not null,
         games_in_lobby      integer,
+        avg_gametime        varchar(8),     #hours:minutes:seconds
+    pieces_taken        integer,
+    avg_movetime        varchar(8),     #hours:minutes:seconds
         avg_moves_per_game  float(4,2),
         users_joined        integer,
-        lobby_id        integer not null,
-#   primary key (lobby_id),                 #FIX
-        foreign key (lobby_id) references lobby(lobby_id)   #FIX
+   primary key (lobby_id),                 
+        foreign key (lobby_id) references lobby(lobby_id)   
 );
  
 create table user_info(
     id          varchar(25) not null,
-    email           varchar(25),
+    email           varchar(40),
     password        char(64),            #char 64 for sha256
     active_lobby        integer,
     unique (email),
-    primary key (id),
-    foreign key (active_lobby) references lobby(lobby_id)
+    primary key (id)
 );
- 
-create table viewer_slot(
-    viewer          varchar(25) not null,
-        lobby_id        integer not null,
-        primary key     (lobby_id),
-        foreign key (lobby_id) references lobby(lobby_id),
-        foreign key (viewer) references user_info(id)
-);
- 
  
 create table player(
-    player_since        char(10) not null,      #MM/DD/YYYY
-    streak          char(2),
     player_id       varchar(25) not null,
     skill           integer,
-    past_punishments    varchar(15),
+    player_since        char(10) not null,      #MM/DD/YYYY
+    streak          char(2),
+    past_punishments    integer,
     primary key (player_id),
     foreign key (player_id) references user_info(id)
 );
  
 create table game_history(
-    avg_movetime        float(4,2),             #minute.second
     match_id        integer not null,
-    match_length        float(4,2),             #hour.minute
+    avg_movetime        varchar(8),     #hours:minutes:seconds
+    match_length        varchar(8),     #hours:minutes:seconds
     replay_id       integer,
     elo_change      integer,
     winner          varchar(15),
-    date_time       char(16),               #MM/DD/YYYY:HH.MM
+    date_time       char(19),               #MM/DD/YYYY@HH:MM:SS
     l_id        integer not null,
     p_id        varchar(25) not null,
     CP_numbermoves  integer,
@@ -108,27 +100,72 @@ create table moderator(
 );
  
 SET foreign_key_checks = 1;
- 
--- danny/gamer123 86333ac84810343f726a58b2d531574c6d42d8e88e34035999040d50541d6fe6
-INSERT INTO `user_info` (`id`, `email`, `password`, `active_lobby`) VALUES ('danny', 'danny@gmail.com', '86333ac84810343f726a58b2d531574c6d42d8e88e34035999040d50541d6fe6', NULL);
- 
--- danny2/gamer321 89b01f18f88e95d8890e70b471f705b33573e712fbe0bebc766104fdd4637888
-INSERT INTO `user_info` (`id`, `email`, `password`, `active_lobby`) VALUES ('danny2', 'danny2@gmail.com', '89b01f18f88e95d8890e70b471f705b33573e712fbe0bebc766104fdd4637888', NULL);
- 
--- user1/gamer1 fda50cb79dc9e7589aa6df29cfb7db27fdd7ed62482ec7c72dde6a28e8fed5e7
-INSERT INTO `user_info` (`id`, `email`, `password`, `active_lobby`) VALUES ('user1', 'user1@gmail.com', 'fda50cb79dc9e7589aa6df29cfb7db27fdd7ed62482ec7c72dde6a28e8fed5e7', NULL);
- 
--- user2/gamer2 f66f158b49308430821743acc354c6419b1c8cdfd5383472d5987fa3ba99c20esh
-INSERT INTO `user_info` (`id`, `email`, `password`, `active_lobby`) VALUES ('user2', 'user2\@gmail.com', 'f66f158b49308430821743acc354c6419b1c8cdfd5383472d5987fa3ba99c20e', NULL);
- 
--- user3/gamer3 49a0a464ee6d980ed2b59d6ca11d985c5ca0695a806f7db6033d11c640f893ac
-INSERT INTO `user_info` (`id`, `email`, `password`, `active_lobby`) VALUES ('user3', 'user3\@gmail.com', '49a0a464ee6d980ed2b59d6ca11d985c5ca0695a806f7db6033d11c640f893ac', NULL);
- 
--- user4/gamer4 99020a9607bfeb1576193e8489981364edca75a5a6f6b9a3d91b9a6af3efa5b5
-INSERT INTO `user_info` (`id`, `email`, `password`, `active_lobby`) VALUES ('user4', 'user4\@gmail.com', '99020a9607bfeb1576193e8489981364edca75a5a6f6b9a3d91b9a6af3efa5b5', NULL);
- 
--- user5/gamer5 1fabc4846ca29959fe6c7bf3796c1a7ed19b5cb37b69ef62d2034b722b96d60d
-INSERT INTO `user_info` (`id`, `email`, `password`, `active_lobby`) VALUES ('user5', 'user5@gmail.com', '1fabc4846ca29959fe6c7bf3796c1a7ed19b5cb37b69ef62d2034b722b96d60d', NULL);
- 
--- user6/gamer6 148d6daa89eaafc744e22984187258c42b1e17d313e2345136de579ecba466f6
-INSERT INTO `user_info` (`id`, `email`, `password`, `active_lobby`) VALUES ('user6', 'user6@gmail.com', '148d6daa89eaafc744e22984187258c42b1e17d313e2345136de579ecba466f6', NULL);
+
+#=====================================================================USERS=====================================================================
+
+insert into user_info (id, email, password, active_lobby) values ('vasquo', 'lvasquez0@yahoo.co.jp', 'c3973cb9194c1b7ac13bc03c3ff1e80dccbaa00bc455dc74679125119472675c', 1);
+insert into user_info (id, email, password, active_lobby) values ('agarrett1', 'dweaver1@artisteer.com', '4904e2f6bfaef020c6fd0f97398c7cd5bb3bd066fb0601646c91505aea195628', 2);
+insert into user_info (id, email, password, active_lobby) values ('rgreene2', 'rgreene2@chicagotribune.com', 'e90bb43dd00e4b9c2feafe726ed3deb15dbda8b10b218ee9c2d3ac7e3c5eecb1', NULL);
+insert into user_info (id, email, password, active_lobby) values ('jwallace3', 'hgreene3@studiopress.com', '72f0d11aaf4dbfa6d90865cc2ddeaee5705ba8487ee69ef7c11e88f6a42a0d7d', NULL);
+insert into user_info (id, email, password, active_lobby) values ('darknight1', 'jcarr4@discuz.net', '6acd86ac6cc424e4b5b0499f95f83238e9d18d445c83e1b5b939b862953a3d52', 2);
+insert into user_info (id, email, password, active_lobby) values ('blawson5', 'arodriguez5@posterous.com', '1eb31c31180e0354a5888bb91af23b34bab8c0a91359ffd1b8163d971a6fa420', 2);
+insert into user_info (id, email, password, active_lobby) values ('enginerd4', 'rhill6@edublogs.org', 'c46c7138780f2bac09b940d85117917869e342952d2ccab9cebf1b7f3064607b', 2);
+insert into user_info (id, email, password, active_lobby) values ('damarsh', 'dmarshall7@amazon.com', '5c478bcf8410a4461f3a15993f9d47f15bfcefe720329f3859eacf2c027e6635', 1);
+insert into user_info (id, email, password, active_lobby) values ('astephens8', 'awagner8@wp.com', '86970ca14a7060b9400e3910d2e8a3fb57ca1f52eb8a307850b727365d680e13', NULL);
+insert into user_info (id, email, password, active_lobby) values ('cramos9', 'cramos9@stanford.edu', '24cd89673ad5ca0c4b6f89ac403cb3788c708530f8919f25542ec8974c532401', 2);
+insert into user_info (id, email, password, active_lobby) values ('bgrahama', 'mmoralesa@sogou.com', 'c4e52bddfb2eb08191ddd4acfdc34339c4c68280236c1f05338d8f5cdddf261c', NULL);
+insert into user_info (id, email, password, active_lobby) values ('rufflife', 'amendozab@sfgate.com', 'f5704a4ba375ebf5c29ce3a02c7b631390c70a7cfe681dc7651328d22c51af47', 1);
+insert into user_info (id, email, password, active_lobby) values ('ljamesc', 'scampbellc@examiner.com', '6be7c8a09fdde88f8fdcb6cc654c5486a21ba3830ff3c6d64ff95d16522f1c54', 2);
+insert into user_info (id, email, password, active_lobby) values ('crusselld', 'iallend@google.pl', '5faa5ad144c7d441e396a613cab60379794889724e9cafa1940875bd243ff855', 2);
+insert into user_info (id, email, password, active_lobby) values ('lwebbe', 'molivere@skype.com', '963aeb2af1bc41e881342f6b553adc6c73e9168bb63b86179b36040c1f6ccdd3', NULL);
+
+#=====================================================================PLAYERS=====================================================================
+
+insert into player (player_since, skill, past_punishments, streak, player_id) values ('12/14/2015', 18, 4, 'L3', 'vasquo');
+insert into player (player_since, skill, past_punishments, streak, player_id) values ('01/30/2016', 46, NULL, 'L2', 'agarrett1');
+insert into player (player_since, skill, past_punishments, streak, player_id) values ('10/05/2014', 1, 2, 'W1', 'rgreene2');
+insert into player (player_since, skill, past_punishments, streak, player_id) values ('04/13/2016', NULL, NULL, NULL, 'jwallace3');
+insert into player (player_since, skill, past_punishments, streak, player_id) values ('05/02/2016', NULL, NULL, NULL, 'darknight1');
+insert into player (player_since, skill, past_punishments, streak, player_id) values ('05/20/2014', 53, NULL, 'L1', 'blawson5');
+insert into player (player_since, skill, past_punishments, streak, player_id) values ('03/02/2016', 23, 1, 'W5', 'damarsh');
+insert into player (player_since, skill, past_punishments, streak, player_id) values ('05/24/2015', 93, 1, 'W6', 'enginerd4');
+insert into player (player_since, skill, past_punishments, streak, player_id) values ('10/19/2015', 6, NULL, 'L6', 'astephens8');
+insert into player (player_since, skill, past_punishments, streak, player_id) values ('08/26/2015', 32, NULL, 'W1', 'cramos9');
+
+#=====================================================================DEVS=====================================================================
+
+insert into developer (fname, lname, dev_id, dev_rights) values ('Alex', 'Morales', 'bgrahama', 'Developer');
+insert into developer (fname, lname, dev_id, dev_rights) values ('Amy', 'Mendoza', 'rufflife', 'Developer');
+insert into developer (fname, lname, dev_id, dev_rights) values ('Larry', 'James', 'ljamesc', 'QA');
+
+#=====================================================================MODS=====================================================================
+
+insert into moderator (fname, lname, mod_id, admin_powers) values ('Christina', 'Russell', 'crusselld', 'Full');
+insert into moderator (fname, lname, mod_id, admin_powers) values ('Lou', 'Webbe', 'lwebbe', 'Limited');
+
+#=====================================================================LOBBY=====================================================================
+
+insert into lobby (lobby_id, min_skill, player_waitlist) values (1, 1, 3);
+insert into lobby (lobby_id, min_skill, player_waitlist) values (2, 25, 1);
+
+#=====================================================================GAMEBOARD TYPE=====================================================================
+
+insert into gameboard_type (name, lobby_id, timer, grid_number, board_color, piece_color) values ('Jungle', 2, '00:12:00', 'A1', 'Green', 'Indigo');
+insert into gameboard_type (name, lobby_id, timer, grid_number, board_color, piece_color) values ('Ocean', 2, '01:00:00', 'F7', 'Blue', 'Pink');
+insert into gameboard_type (name, lobby_id, timer, grid_number, board_color, piece_color) values ('Rose', 2, '12:00:00', 'B3', 'Red', 'Red');
+insert into gameboard_type (name, lobby_id, timer, grid_number, board_color, piece_color) values ('Unicorn', 1, '00:03:00', 'A2', 'Violet', 'Turquoise');
+insert into gameboard_type (name, lobby_id, timer, grid_number, board_color, piece_color) values ('Basketball', 2, '00:05:00', 'B4', 'Orange', 'Purple');
+insert into gameboard_type (name, lobby_id, timer, grid_number, board_color, piece_color) values ('Ocean', 1, '00:20:00', 'C4', 'Teal', 'Blue');
+
+#=====================================================================LOBBY STATS=====================================================================
+
+insert into lobby_stats (lobby_id, games_in_lobby, avg_gametime, pieces_taken, avg_movetime, avg_moves_per_game, users_joined) values (1, 53, '03:09:16', 762, '00:11:26', 22.7, 112);
+insert into lobby_stats (lobby_id, games_in_lobby, avg_gametime, pieces_taken, avg_movetime, avg_moves_per_game, users_joined) values (2, 22, '04:03:52', 339, '00:17:28', 31.08, 52);
+
+#=====================================================================GAME HISTORY=====================================================================
+
+insert into game_history (match_id, avg_movetime, match_length, replay_id, elo_change, winner, date_time, l_id, p_id, CP_numbermoves, CP_ID, cp_startpos, cp_endpos) values (70730, '00:11:43', '02:26:00', 48396, 3, 'blawson5', '02/14/2016@03:07:37', 1, 'blawson5', 26, 12, 'A2', 'B4');
+insert into game_history (match_id, avg_movetime, match_length, replay_id, elo_change, winner, date_time, l_id, p_id, CP_numbermoves, CP_ID, cp_startpos, cp_endpos) values (77091, '03:08:24', '44:05:37', 70145, -1, 'enginerd4', '01/10/2016@14:09:08', 2, 'blawson5', 48, 5, 'C3', 'D9');
+insert into game_history (match_id, avg_movetime, match_length, replay_id, elo_change, winner, date_time, l_id, p_id, CP_numbermoves, CP_ID, cp_startpos, cp_endpos) values (28502, '00:37:09', '04:07:45', 46349, 4, 'damarsh', '11/03/2015@15:01:22', 2, 'damarsh', 33, 1, 'D2', 'A4');
+insert into game_history (match_id, avg_movetime, match_length, replay_id, elo_change, winner, date_time, l_id, p_id, CP_numbermoves, CP_ID, cp_startpos, cp_endpos) values (42450, '01:03:17', '23:42:10', 70093, 3, 'vasquo', '11/06/2015@12:54:55', 2, 'vasquo', 58, 31, 'B2', 'C4');
+insert into game_history (match_id, avg_movetime, match_length, replay_id, elo_change, winner, date_time, l_id, p_id, CP_numbermoves, CP_ID, cp_startpos, cp_endpos) values (66070, '00:12:09', '05:11:05', 79598, -5, 'rgreene2', '06/11/2015@23:45:08', 1, 'cramos9', 32, 18, 'D2', 'D4');
